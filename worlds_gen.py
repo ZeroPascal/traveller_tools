1cd9bf8e77ce031d9dea62f93a0fd440bffe5376
ZeroPascal <drew.t.winston@gmail.com>
2024-12-09 14:04:50 -0600

    law and starport

--- worlds_gen.py
+++ worlds_gen.py
@@ -11,8 +11,10 @@ def roll(dice=1,type=6, mods=0,stat_mod=0,stat_type=0):
     #print("Rolling",dice,'d',type, "Min",min,"Max",max)
     return(random.randint(min,max))
 
-def printClass(name,number):
-    print('\033[1m'+str(name),'Class',f'{number:x}'.upper()+"-",'\033[0m')
+def printClass(name,number,ignoreHex=False):
+    if(not ignoreHex):
+        number = f'{number:x}'.upper()
+    print('\033[1m'+str(name),'Class',str(number)+"-",'\033[0m')
 #Import Data
 input = open("worlds.json", "r")
 tables=json.load(input)
@@ -144,7 +146,7 @@ if(goverment>=10):
 
 fraction_count=roll(1,3,fraction_mod)
 
-printClass('Fractions',fraction_count)
+print(' Fractions',fraction_count)
 
 fractions = []
 for f in range(fraction_count):
@@ -155,49 +157,64 @@ for f in range(fraction_count):
     )
 
 for f in fractions:
-    print(" ",f)
+    print("     ",f)
 law=roll(2,6,goverment-7)
 
 printClass('Law Level',law)
 
+starport_mod =0
+if population>=10:
+    starport_mod=2
+elif population<=2:
+    starport_mod=-2
+else:
+    match population:
+        case 8,9:
+            starport_mod=1
+        case 3,4:
+            starport_mod=-1
+
+starport = tables['STARPORT_CLASS'][str(roll(2,6,starport_mod))]
+
+printClass('Starport',starport,True)
 tech=roll(1,6,0)
 
-trade_code =''
+trade_code =[]
 
 if(4<= atmo >=9 and 4<=hydro>=8 and 5<=population>=7):
-    trade_code+='Ag'
+    trade_code.append('Ag')
 if(size==0 and atmo==0 and hydro==0):
-    trade_code+='As'
+    trade_code.append('As')
 if(population==0 and goverment==0 and law==0):
-    trade_code+='Ba'
+    trade_code.append('Ba')
 if(2<=atmo>=9 and hydro==0):
-    trade_code+='De'
+    trade_code.append('De')
 if(atmo>=10 and hydro>=1):
-    trade_code+='Fl'
+    trade_code.append('Fl')
 if(6<=size>=8 and (5<=atmo>=6 or atmo==8) and 5<=hydro>=7):
-    trade_code+='Ga'
+    trade_code.append('Ga')
 if(population>=9):
-    trade_code+='Hi'
+    trade_code.append('Hi')
 if(tech>=12):
-    trade_code+='Ht'
+    trade_code.append('Ht')
 if(0<=atmo>=1 and hydro>=1):
-    trade_code+='Ic'
+    trade_code.append('Ic')
 if(population>=9 and (0<=atmo>=2 or atmo==4 or atmo==7 or 9<=atmo>=12)):
-    trade_code+='In'
+    trade_code.append('In')
 if(1<=population>=3):
-    trade_code+='Lo'
+    trade_code.append('Lo')
 if(tech<=5):
-    trade_code+='Lt'
+    trade_code.append('Lt')
 if(0<=atmo>=3 and 0<=hydro>=3 and population>=6):
-    trade_code+='Na'
+    trade_code.append('Na')
 if(2<=atmo>=5 and 0<=hydro>=3):
-    trade_code+='Po'
+    trade_code.append('Po')
 if((atmo==6 or atmo==8) and 6<=population>=8 and 4<=goverment>=9):
-    trade_code+='Ri'
+    trade_code.append('Ri')
 if(atmo==0):
-    trade_code+='Va'
+    trade_code.append('Va')
 if((3<=atmo>=9 or atmo>=13) and hydro>=-0):
-    trade_code+='Wa'
+    trade_code.append('Wa')
     
 print('Trade Code',trade_code)
 
